e70c075f84e44f0dd47e2441b5111071adee7a77
pengshiyu.cc <pengshiyu.cc@bytedance.com>
2022-03-12 19:39:22 +0800

    first commit

---
+++ build.rs
@@ -0,0 +1,8 @@
+fn main() {
+    prost_build::Config::new()
+        .bytes(&["."])
+        .type_attribute(".", "#[derive(PartialOrd)]")
+        .out_dir("src/pb")
+        .compile_protos(&["abi.proto"], &["."])
+        .unwrap();
+}
